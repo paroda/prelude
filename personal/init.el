9427a0ad68c384cffd247bf91e0cea8423e30c54
Pradyumna Parida <paroda@gmail.com>
2022-06-27 18:19:17 +0530

    update hotkeys

--- personal/init.el
+++ personal/init.el
@@ -291,20 +291,20 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(progn
-  ;; set arrow keys in isearch
-  ;; left/right is backward/forward
-  ;; up/down is history
-  ;; press Return to exit
-  (define-key isearch-mode-map (kbd "<up>") 'isearch-ring-retreat)
-  (define-key isearch-mode-map (kbd "<down>") 'isearch-ring-advance)
-  (define-key isearch-mode-map (kbd "<left>") 'isearch-repeat-backward)
-  (define-key isearch-mode-map (kbd "<right>") 'isearch-repeat-forward)
-  (define-key minibuffer-local-isearch-map (kbd "<left>")
-    'isearch-reverse-exit-minibuffer)
-  (define-key minibuffer-local-isearch-map (kbd "<right>")
-    'isearch-forward-exit-minibuffer))
-(global-set-key (kbd "C-S-s") 'isearch-forward-symbol-at-point)
+;; set arrow keys in isearch
+;; left/right is backward/forward
+;; up/down is history
+;; press Return to exit
+(define-key isearch-mode-map (kbd "<up>") 'isearch-ring-retreat)
+(define-key isearch-mode-map (kbd "<down>") 'isearch-ring-advance)
+(define-key isearch-mode-map (kbd "<left>") 'isearch-repeat-backward)
+(define-key isearch-mode-map (kbd "<right>") 'isearch-repeat-forward)
+(define-key minibuffer-local-isearch-map (kbd "<left>")
+  'isearch-reverse-exit-minibuffer)
+(define-key minibuffer-local-isearch-map (kbd "<right>")
+  'isearch-forward-exit-minibuffer)
+(global-set-key (kbd "C-S-s") 'isearch-forward-thing-at-point)
+(setq isearch-allow-motion t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; keep scratch buffer on restart
